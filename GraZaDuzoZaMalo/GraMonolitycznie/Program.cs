b5380461c265d7b9784c6ad3719054696e01fe59
szczudlok <potos16@gmail.com>
2019-05-04 19:25:30 +0200

    Podstawa wersji

--- GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
+++ GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
@@ -11,31 +11,67 @@ namespace GraMonolitycznie
         static void Main(string[] args)
         {
             /*
-            Console.Write("witaj!");
+            Console.WriteLine("Witaj!");
             Console.Write("Podaj swoje imię: ");
             string x = Console.ReadLine();
             Console.WriteLine($"Witaj, {x}");
             */
-            //1.Komputer losuje liczbę
+
+            // 1. Komputer losuje liczbę
             Random generator = new Random();
             int wylosowana = generator.Next(1, 101);
             Console.WriteLine("Wylosowałem liczbę od 1 do 100. \n Odgadnij ją");
+
 #if(DEBUG)
             Console.WriteLine(wylosowana);
 #endif
-            //2.Człowiek proponuje rozwiązanie 
-            Console.Write("Podaj swoją propozycje: ");
-            string tekst = Console.ReadLine();
-            int propozycja = Convert.ToInt32(tekst);
-            Console.WriteLine($"Przyjąłem wartość {propozycja}");
-            //3.Komputer ocenia propozycje 
-            if(propozycja < wylosowana)
-                Console.WriteLine("za mało");
-            else if(propozycja > wylosowana)
-                Console.WriteLine("za dużo");
-            else 
-                Console.WriteLine("trafiono");
-            Console.WriteLine("Koniec Gry");
+
+            //wykonuj
+            bool trafiono = false; //wartownik (zwany czasami flagą)
+            do
+            {
+                #region Krok 2. Człowiek proponuje rozwiązanie
+                Console.Write("Podaj swoją propozycję: ");
+                string tekst = Console.ReadLine();
+                if (tekst.ToLower() == "x")
+                    break;
+
+                int propozycja = 0;
+                try
+                {
+                    propozycja = Convert.ToInt32(tekst);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Nie podano liczby!");
+                    continue;
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Liczba nie mieści się w rejestrze!");
+                    continue;
+                }
+
+                Console.WriteLine($"Przyjąłem wartość {propozycja}");
+                #endregion
+
+
+                #region Krok 3. Komputer ocenia propozycję
+                if (propozycja < wylosowana)
+                    Console.WriteLine("za mało");
+                else if (propozycja > wylosowana)
+                    Console.WriteLine("za dużo");
+                else
+                {
+                    Console.WriteLine("trafiono");
+                    trafiono = true;
+                }
+                #endregion
+            }
+            while (!trafiono);
+            //do momentu trafienia
+
+            Console.WriteLine("Koniec gry");
         }
     }
 }
---
+++ GraZaDuzoZaMalo/GraProceduralnie/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraProceduralnie
+{
+    class Program
+    {
+        /// <summary>
+        /// Wczytuje dane(liczba całkowita) z konsoli.Jeśli 'x' zgłasza wyjątek
+        /// </summary>
+        /// <param name="prompt">Tekst zachęty wyświetlany uzytkownikowi</param>
+        /// <returns>gdy użytkownik poda x</returns>
+        private static int WczytajDane(string prompt)
+        {
+             
+            do
+            {
+                Console.WriteLine(prompt);
+                string tekst = Console.ReadLine();
+                if (tekst.ToLower() == "x")
+                    throw new InvalidOperationException(); //todo 
+                int propozycja = 0;
+
+                try
+                {
+                    propozycja = Convert.ToInt32(tekst); // int.Parse(tekst)
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Nie podano liczby!");
+
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Liczba nie mieści się w rejestrze! Spróbuj jeszcze raz! ");
+
+                }
+            }
+
+            while (true);
+            return propozycja;
+        }
+
+        static int Losuj(int min, int max)
+        {
+            Random generator = new Random();
+            int wylosowana = generator.Next(min, max + 1);
+            return wylosowana;
+        }
+
+        private static int Wylosowana = 0;
+
+        private static string Ocena(int propozycja)
+        {
+            if (propozycja < Wylosowana)
+                Console.WriteLine("za mało");
+            else if (propozycja > Wylosowana)
+                Console.WriteLine("za dużo");
+            else
+            {
+                Console.WriteLine("trafiono");
+                trafiono = true;
+            }
+        }
+       
+      
+
+        static void Main(string[] args)
+        {
+
+
+            // 1. Komputer losuje liczbę
+            Wylosowana = Losuj(1, 100); // może być zgłoszony wyjątek, gdy min > max
+            Console.WriteLine("Wylosowałem liczbę od 1 do 100. \n Odgadnij ją");
+
+#if(DEBUG)
+            Console.WriteLine(Wylosowana);
+#endif
+
+            //wykonuj
+            int propozycja = 0;
+            do
+            {
+                try
+                {
+                    int propozycja = WczytajDane("Podaj swoją propozycję (x - gdy koniec");
+                }
+                catch(InvalidOperationException);
+                {
+                    Console.WriteLine("Szkoda że kończymy");
+                    return;
+                }
+
+                Console.WriteLine($"Przyjąłem wartość {propozycja}");
+
+                string ocena = Ocena(propozycja);
+                Console.WriteLine(ocena);
+                if (ocena == "trafiono")
+                    break;
+
+                }
+            while (!trafiono);
+            //do momentu trafienia
+
+            Console.WriteLine("Koniec gry");
+        }
+    }
+}
